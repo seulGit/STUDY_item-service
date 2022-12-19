7fe9b1a6b2316282ba0955131e33a3df9f7337ce
seulGit <seulgi9421@gmail.com>
2022-12-19 14:47:14 +0900

    [item-service] 신규 커밋 3

---
+++ item-service/src/main/java/hello/itemservice/domain/item/Item.java
@@ -0,0 +1,56 @@
+package hello.itemservice.domain.item;
+
+public class Item {
+	
+	private Long id;
+	private String itemName;
+	private Integer price;
+	private Integer quantity;
+	
+	public Item() {
+	}
+
+	public Item(String itemName, Integer price, Integer quantity) {
+		this.itemName = itemName;
+		this.price = price;
+		this.quantity = quantity;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	@Override
+	public String toString() {
+		return "Item [id=" + id + ", itemName=" + itemName + ", price=" + price + ", quantity=" + quantity + "]";
+	}
+	
+}
